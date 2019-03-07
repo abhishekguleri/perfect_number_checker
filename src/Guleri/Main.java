686730b09da80a76bc6c3bcdafeb0febaf7bc65a
abhishekguleri <abhishekguleri24@gmail.com>
2019-03-07 23:06:26 +0530

    Initial commit

---
+++ src/Guleri/Main.java
@@ -0,0 +1,21 @@
+package Guleri;
+
+public class Main {
+
+    public static void main(int number) {
+                if(number<1){
+                    System.out.println("Invalid Value");
+                }
+                int i=1;
+                while(i<=number){
+                    if(number%i==0){
+
+                        System.out.println(i);}
+                    i++;
+                }
+
+            }
+        }
+	// write your code here
+    }
+}
